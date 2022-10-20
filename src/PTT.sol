ea0e8f05e93d29430c91c655256517fb3618dc33
Sam Larsen <sam@slarsen.io>
2022-10-20 01:49:43 -0400

    👷‍♂️ Improved isApprovedForTransfer

--- src/PTT.sol
+++ src/PTT.sol
@@ -6,13 +6,15 @@ import "./IPTT.sol";
 import "@0xver/solver/library/Merkle.sol";
 
 /// @title Physically Transferrable Token (PTT) implementation
+/// @dev This is a non-optimized implementation
 contract PTT is IPTT {
     mapping(address => uint256) public override(IPTT) balanceOf;
     mapping(uint256 => address) public override(IPTT) ownerOf;
     mapping(uint256 => address) public override(IPTT) initialized;
     mapping(uint256 => mapping(address => uint256)) public override(IPTT) offer;
+    mapping(uint256 => mapping(bytes32 => uint256)) private _processedMap;
+    mapping(uint256 => uint256) private _lastProcessed;
     mapping(uint256 => bytes32) private _tokenRootMap;
-    mapping(bytes32 => bool) private _processedMap;
     uint256 private _currentTokenId;
 
     function mint(bytes32 _root) public {
@@ -27,10 +29,10 @@ contract PTT is IPTT {
         string memory _code,
         bytes32[] calldata _proof
     ) public view override(IPTT) returns (bool) {
-        bytes32 leaf = keccak256(abi.encodePacked(_code));
-        if (_processedMap[leaf] == true) {
+        if (Strings.stringify(_code) <= _lastProcessed[_tokenId]) {
             return false;
         } else {
+            bytes32 leaf = keccak256(abi.encodePacked(_code));
             return Merkle.verify(_proof, _tokenRoot(_tokenId), leaf);
         }
     }
@@ -41,8 +43,8 @@ contract PTT is IPTT {
         emit Initialize(ownerOf[_tokenId], msg.sender, _tokenId, msg.value);
     }
 
-    function revertTransaction(uint256 _tokenId) public payable {
-        require(initialized[_tokenId] != address(0));
+    function revertTransaction(uint256 _tokenId) public {
+        require(initialized[_tokenId] == address(0));
         uint256 amount = offer[_tokenId][msg.sender];
         delete offer[_tokenId][msg.sender];
         (bool success, ) = payable(msg.sender).call{value: amount}("");
@@ -84,7 +86,7 @@ contract PTT is IPTT {
             balanceOf[_to] += 1;
         }
         ownerOf[_tokenId] = _to;
-        initialized[_tokenId] = address(0);
+        delete initialized[_tokenId];
         uint256 amount = offer[_tokenId][_to];
         (bool success, ) = payable(_from).call{value: amount}("");
         require(success, "ETHER_TRANSFER_FAILED");
@@ -109,6 +111,30 @@ contract PTT is IPTT {
             Merkle.verify(_proof, _tokenRoot(_tokenId), leaf),
             "INVALID_PROOF"
         );
-        _processedMap[leaf] = true;
+        uint128 num = uint128(Strings.stringify(_code));
+        _processedMap[_tokenId][leaf] = num;
+        _lastProcessed[_tokenId] = num;
+    }
+}
+
+library Strings {
+    function stringify(string memory _string)
+        internal
+        pure
+        returns (uint256 number)
+    {
+        for (uint256 i = 0; i < bytes(_string).length; i++) {
+            if (
+                (uint8(bytes(_string)[i]) - 48) < 0 ||
+                (uint8(bytes(_string)[i]) - 48) > 9
+            ) {
+                return 0;
+            }
+            number +=
+                (uint8(bytes(_string)[i]) - 48) *
+                10**(bytes(_string).length - i - 1);
+        }
+
+        return number;
     }
 }
